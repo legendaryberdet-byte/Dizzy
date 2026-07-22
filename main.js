50e8df7a8fe61227fc523292ae3a106c63aec334
legendaryberdet-byte <legendaryberdet@gmail.com>
2026-07-22 20:16:51 -0300

    por favor or 😭

--- main.js
+++ main.js
@@ -178,15 +178,6 @@ if (result.leveledUp) {
 });
 
 // Comando: Ver XP/Level (prefix command)
-
-client.on('messageCreate', async (message) => {
-  console.log("Mensagem recebida:", message.content);
-
-  if (message.author.bot || !message.guild) return;
-
-});
-
-
 client.on('messageCreate', async (message) => {
   if (message.author.bot || !message.guild) return;
 
