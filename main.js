a9b4c8024293182cda89dfb40368b57e989f29e2
legendaryberdet-byte <legendaryberdet@gmail.com>
2026-07-23 22:34:38 -0300

    limpar comandos

--- main.js
+++ main.js
@@ -509,5 +509,26 @@ client.on("interactionCreate", async interaction => {
 
 });
 
+const { REST, Routes } = require('discord.js');
+
+const rest = new REST({ version: '10' })
+  .setToken(process.env.DISCORD_TOKEN);
+
+(async () => {
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: [] }
+    );
+
+    console.log("✅ Comandos slash removidos!");
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
 // Conectar ao Discord
 client.login(process.env.DISCORD_TOKEN);
